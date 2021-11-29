5d4643a8bf36bf4067460724ed5ccb2aafc47dc9
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-11-29 12:44:31 +0100

    a lot of changes

--- packages/pwa/src/index.sw.ts
+++ packages/pwa/src/index.sw.ts
@@ -1,21 +1,20 @@
 /* eslint-env serviceworker */
 import { initialize as initGA } from 'workbox-google-analytics';
-import { matchPrecache, precacheAndRoute } from 'workbox-precaching';
-import { registerRoute, setCatchHandler } from 'workbox-routing';
+import { matchPrecache } from 'workbox-precaching';
+import { setCatchHandler } from 'workbox-routing';
+import { CacheFirst } from 'workbox-strategies';
 import { enable as navigationPreloadEnable } from 'workbox-navigation-preload';
-import { setCacheNameDetails, clientsClaim } from 'workbox-core';
-import { offlineFallback, googleFontsCache } from 'workbox-recipes';
-import { CacheableResponsePlugin } from 'workbox-cacheable-response';
-import { NetworkOnly, NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { setCacheNameDetails, clientsClaim, RouteHandlerCallbackOptions } from 'workbox-core';
+import { googleFontsCache, warmStrategyCache } from 'workbox-recipes';
 
-import Logger from './Logger';
-import { resetCaches } from './caches';
+import Analytics from './Analytics';
+import { registerRoutes, resetCaches } from './routes';
 
-import type { RouteMatchCallbackOptions, RouteHandlerCallbackOptions } from 'workbox-core';
+import type { SWConfig } from '../types/sw';
 
 declare var self: PlatformSWScope;
 
-self.logger = self.logger || new Logger();
+self.analytics = self.analytics || new Analytics();
 
 export async function init(config: SWConfig) {
 	const { options } = config;
@@ -23,17 +22,16 @@ export async function init(config: SWConfig) {
 		activationHandler();
 		skipWaitHandler();
 
-		const cacheNames: ChacheNames = {
-			images: `images_v${OFFLINE_VERSION}`,
-			offline: `offline_v${OFFLINE_VERSION}`,
-			pages: `pages_v${OFFLINE_VERSION}`,
-			static: `static_${OFFLINE_VERSION}`,
-			fonts: `fonts_${OFFLINE_VERSION}`,
-		};
+		const cacheNames: string[] = [];
 
-		// workbox.setConfig({ debug: false });
+		resetCaches(cacheNames);
 
-		resetCaches(Object.values(cacheNames));
+		warmStrategyCache({
+			urls: config.warm?.urls || ['/offline.html', '/placeholder.svg'],
+			strategy: config.warm?.strategy || new CacheFirst(),
+		});
+
+		registerRoutes();
 
 		navigationPreloadEnable();
 
@@ -45,19 +43,26 @@ export async function init(config: SWConfig) {
 
 		clientsClaim();
 
-		if (options.cacheGoogleFonts || true) {
+		setCatchHandler(e =>
+			catchHandler(e, {
+				font: '',
+				html: '',
+				image: '',
+				script: '',
+			})
+		);
+
+		if (options.cacheGoogleFonts ?? true) {
 			googleFontsCache();
 		}
 
-		if (config.enableGoogleAnalytics) {
+		if (options.enableGoogleAnalytics ?? true) {
 			initGA();
 		}
 
-		// offlineFallback({
-		// pageFallback: '/_offline.html',
-		// });
-
-		catchHandler();
+		if (options.enableNaviagtionPreload ?? true) {
+			navigationPreloadEnable();
+		}
 	} catch (error) {
 		console.error('sw: ', error);
 	}
@@ -77,14 +82,52 @@ function skipWaitHandler() {
 	});
 }
 
-function catchHandler() {
-	setCatchHandler(async e => {
-		// Return the precached offline page if a document is being requested
-		if (e.request.destination === 'document') {
-			return (await matchPrecache('/offline.html')) || Response.error();
-		}
+interface FallbackUrls {
+	html: string;
+	image: string;
+	font: string;
+	script: string;
+}
 
-		return Response.error();
-		// return new Response(null, { status: 404 });
-	});
+async function catchHandler(e: RouteHandlerCallbackOptions, fallbackUrls: FallbackUrls) {
+	// The FALLBACK_URL entries must be added to the cache ahead of time, either
+	// via runtime or precaching. If they are precached, then call
+	// `matchPrecache(FALLBACK_URL)` (from the `workbox-precaching` package)
+	// to get the response from the correct cache.
+	//
+	// Use event, request, and url to figure out how to respond.
+	// One approach would be to use request.destination, see
+	// https://medium.com/dev-channel/service-worker-caching-strategies-based-on-request-types-57411dd7652c
+	switch (e.request.destination) {
+		case 'document': {
+			// If using precached URLs:
+			// return matchPrecache(FALLBACK_HTML_URL);
+			const match = await caches.match(fallbackUrls.html);
+			if (match) return match;
+			break;
+		}
+		case 'image': {
+			// If using precached URLs:
+			// return matchPrecache(FALLBACK_IMAGE_URL);
+			const match = await caches.match(fallbackUrls.image);
+			if (match) return match;
+			break;
+		}
+		case 'font': {
+			// If using precached URLs:
+			// return matchPrecache(FALLBACK_FONT_URL);
+			const match = await caches.match(fallbackUrls.font);
+			if (match) return match;
+			break;
+		}
+		case 'script': {
+			// If using precached URLs:
+			// return matchPrecache(FALLBACK_IMAGE_URL);
+			const match = await caches.match(fallbackUrls.script);
+			if (match) return match;
+			break;
+		}
+	}
+	// If we don't have a fallback, just return an error response.
+	return Response.error();
 }
